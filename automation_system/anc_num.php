b696f07a227655fa7810166424464b57a775db01
saloni1996 <salonibhartia1996@gmail.com>
2018-02-10 20:43:13 +0530

    Added ui

---
+++ automation_system/anc_num.php
@@ -0,0 +1,6 @@
+
+<form action="submit_anc.php" method=post >
+  <h4>Enter the anc card number:</h4>
+    <input style='height:32px' type=text name=ques>
+    &nbsp<input type=submit name=enter class='btn btn-primary' value='Enter'>
+</form>
---
+++ automation_system/signup.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Option Selection</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+  <!--===============================================================================================-->
+  </head>
+<body>
+
+  <span class="login100-form-title p-b-70">
+    Select an option
+  </span>
+
+
+  <button class="addnew-form-btn" formaction="new_patient.php">
+    Add new
+  </button>
+
+  <button class="addnew-form-btn" formaction="anc_num.php">
+    View Previous
+  </button>
+
+</body>
+</html>
