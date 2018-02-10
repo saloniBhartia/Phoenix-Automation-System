c3abd2101aeb0a30d1b926d99d912cc79871ad5b
saloni1996 <salonibhartia1996@gmail.com>
2018-02-11 00:24:53 +0530

    Added the previous patients view

---
+++ automation_system/view_patients_option.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Option Selection</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+  <!--===============================================================================================-->
+  </head>
+<body>
+
+  <span class="login100-form-title p-b-70">
+    Select an option
+  </span>
+
+<form method="post" action="view_history.php">
+  <button class="addnew-form-btn" >
+   View History
+  </button>
+</form>
+
+	<form method="post" action="modify_details.php">
+		<button class="addnew-form-btn">
+			Update/Edit the details
+		</button>
+	</form>
+
+
+</body>
+</html>
